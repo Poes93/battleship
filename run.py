fd4ba98a7f27fc806c8ae8004063f2573da7ccd2
Daniel Ahlberg <141239673+Poes93@users.noreply.github.com>
2023-10-06 15:58:12 +0200

    add start of game parameters

--- run.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to Battleship!") 
-print("You will be playing against the computer.")
-print("You will be prompted to enter the coordinates of your ships.")
-print("The computer will then place its ships.")
-print("You will then take turns guessing the coordinates of the ships.")
-print("The first person to sink all the ships wins!")
-print("Good luck!")
-
