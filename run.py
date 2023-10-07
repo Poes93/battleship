cbec1b49b78fd7020e60ea222e1264d457b431c7
Daniel Ahlberg <141239673+Poes93@users.noreply.github.com>
2023-10-07 07:57:13 +0200

    printing the board and generating randomly placed ships

--- run.py
+++ run.py
@@ -6,9 +6,20 @@ class battleshipgame:
         #initialize the game parameters with grid size and number of ships
         self.grid_size = 10
         self.num_of_ships = 5
-        
+
         # fill the board with 0's
         self.player_board = [['0'] * self.grid_size for i in range(self.grid_size)]
         self.computer_board = [['0'] * self.grid_size for i in range(self.grid_size)]
+
+        # place the ships on the board
+        self.place_ships(self.player_board)
+        self.place_ships(self.computer_board)
+
+
 main()
-    game = battleshipgame()        
+    game = battleshipgame()    
+    game.play()    
+
+
+    #starting the game and printing the board
+print "Let's play Battleship!"
