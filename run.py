bd882d026826cbd8ff3568eb1ee71d892827fee0
Daniel Ahlberg <141239673+Poes93@users.noreply.github.com>
2023-10-14 22:16:14 +0200

    fix when game finished the when pressing yes, then the bugs started if 0 was pressed as grid

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import random
 import sys
-from colorama import Fore, init
+from colorama import Fore, Style, init
 init(autoreset=True)
 
 
@@ -17,7 +17,8 @@ class BattleshipGame:
 
         if self.num_of_ships > self.grid_size * self.grid_size:
             raise ValueError(
-                Fore.RED + "Number of ships can't exceed the grid size"
+                Fore.RED + Style.BRIGHT +
+                "Number of ships can't exceed the grid size"
                 )
 
         # fill the board with 0's
@@ -68,15 +69,16 @@ class BattleshipGame:
         Check if the player has already guessed this spot
         """
         if self.computer_board[row][col] in ["H", "M"]:
-            print(Fore.RED + "You already guessed that spot, try again")
+            print(Fore.RED + Style.BRIGHT +
+                  "You already guessed that spot, try again")
             return False
 
         # Process the player's guess
         if self.computer_board[row][col] == "X":
-            print(Fore.RED + "Hit!")
+            print(Fore.RED + Style.BRIGHT + "Hit!")
             self.computer_board[row][col] = "H"
         else:
-            print(Fore.BLUE + "Miss!")
+            print(Fore.BLUE + Style.BRIGHT + "Miss!")
             self.computer_board[row][col] = "M"
         return True
 
@@ -92,12 +94,14 @@ class BattleshipGame:
                 break
         if self.player_board[row][col] == "X":
             print(
-                Fore.RED + f"Computer has hit your ship at ({row}, {col})"
+                Fore.RED + Style.BRIGHT +
+                f"Computer has hit your ship at ({row}, {col})"
                 )
             self.player_board[row][col] = "H"
         else:
             print(
-                Fore.BLUE + f"Computer has missed your ship at ({row}, {col})"
+                Fore.BLUE + Style.BRIGHT +
+                f"Computer has missed your ship at ({row}, {col})"
                 )
             self.player_board[row][col] = "M"
 
@@ -124,17 +128,19 @@ class BattleshipGame:
                         or guess_col < 0
                         or guess_col >= self.grid_size
                     ):
-                        print(Fore.RED + "That's outside the grid")
+                        print(Fore.RED + Style.BRIGHT +
+                              "That's outside the grid")
                         continue
 
                     if self.player_guess(guess_row, guess_col):
                         break
                 except ValueError:
-                    print(Fore.RED + "Please enter a number")
+                    print(Fore.RED + Style.BRIGHT + "Please enter a number")
 
             # Check if the player has won
             if not any("X" in row for row in self.computer_board):
-                print(Fore.GREEN + "You sank the computers ships!")
+                print(Fore.GREEN + Style.BRIGHT +
+                      "You sank the computers ships!")
                 break
 
             # Computer's turn
@@ -143,78 +149,56 @@ class BattleshipGame:
 
             # Check if the computer has won
             if not any("X" in row for row in self.player_board):
-                print(Fore.RED + "The computer has sunken all your ships!")
+                print(Fore.RED + Style.BRIGHT +
+                      "The computer has sunken all your ships!")
                 break
 
 
 if __name__ == "__main__":
     # Print a welcome message and instructions
-    print(Fore.YELLOW + "===================================")
-    print(Fore.YELLOW + "Welcome to the Battleship game!")
-    print(Fore.YELLOW + "===================================")
-    print(Fore.CYAN + """
+    print(Fore.YELLOW + Style.BRIGHT + "===================================")
+    print(Fore.YELLOW + Style.BRIGHT + "Welcome to the Battleship game!")
+    print(Fore.YELLOW + Style.BRIGHT + "===================================")
+    print(Fore.CYAN + Style.BRIGHT + """
     Instructions:
     1. You will be asked to enter the grid size and number of ships.
     2. Take turns to guess the position of each other's ships.
     3. You can guess a position by entering the row and column number
     where 0 is the first row/column.
     4. The aim is to sink all the enemy's ships before they sink yours.
-    5. 'X' represents a ship.
-    6. 'H' represents a hit.
-    7. 'M' represents a miss.
-    8. '0' represents an unknown position.
+    5. 'X' = ship.
+    6. 'H' = hit.
+    7. 'M' = miss.
+    8. '0' = unknown.
     """)
 
     while True:
-        try:
-            size = int(input("Enter the grid size: "))
-            if size <= 0:   # check if the size is valid
-                print(Fore.RED + "Grid size should be greater than 0.")
-                continue
-            break
-        except ValueError:
-            print(Fore.RED + "Please enter a number")
+        while True:
+            try:
+                size = int(input(Fore.YELLOW + Style.BRIGHT + "Enter the grid size: "))
+                if size <= 0:   # check if the size is valid
+                    print(Fore.RED + Style.BRIGHT + "Grid size should be greater than 0.")
+                    continue
+                break
+            except ValueError:
+                print(Fore.RED + Style.BRIGHT + "Please enter a number")
 
-    while True:
-        try:
-            num_of_ships = int(input("Enter the number of ships: "))
-            if num_of_ships <= 0:   # check if the size is valid
-                print(Fore.RED + "Number of ships should be greater than 0.")
-                continue
-            game = BattleshipGame(size, num_of_ships)
-            break
-        except ValueError as e:
-            if "Number of ships can't exceed the grid size" in str(e):
-                print(Fore.RED + "You have entered more ships than the grid!")
-            else:
-                print(Fore.RED + "Please enter a number")
+        while True:
+            try:
+                num_of_ships = int(input(Fore.YELLOW + Style.BRIGHT + "Enter the number of ships: "))
+                if num_of_ships <= 0 or num_of_ships > size*size:   # check if the number of ships is valid
+                    print(Fore.RED + Style.BRIGHT + "Number of ships should be greater than 0 and not exceed grid capacity.")
+                    continue
+                game = BattleshipGame(size, num_of_ships)
+                break
+            except ValueError as e:
+                if "Number of ships can't exceed the grid size" in str(e):
+                    print(Fore.RED + Style.BRIGHT + "You have entered more ships than the grid!")
+                else:
+                    print(Fore.RED + Style.BRIGHT + "Please enter a number")
 
-    while True:
         game.play()
         # Ask if the player wants to play again
         play_again = input("Do you want to play again? (yes/no): ")
-        if play_again.lower() == "yes":
-            while True:
-                try:
-                    size = int(input("Enter the new grid size: "))
-                    break
-                except ValueError:
-                    print(Fore.RED + "Please enter a number")
-            # Check if the size is valid for the next game
-            while True:
-                try:
-                    num_of_ships = (
-                        int(input("Enter the new number of ships: "))
-                        )
-                    game = BattleshipGame(size, num_of_ships)
-                    break
-                except ValueError as e:
-                    if "Number of ships can't exceed the grid size" in str(e):
-                        print(Fore.RED + (
-                            "You have entered more ships than the grid!")
-                              )
-                    else:
-                        print(Fore.RED + "Please enter a number")
-        else:
+        if play_again.lower() != "yes":
             break
-        sys.exit()
